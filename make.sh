43835a11cffa2d6a51ed54fb7d2066becd0a7ece
Dan Sackett <danesackett@gmail.com>
2013-03-14 08:57:56 -0400

    Add gitconfig

--- make.sh
+++ make.sh
@@ -5,10 +5,12 @@
 ############################
 
 ########## Variables
-
-dir=~/dotfiles                          # dotfiles directory
-olddir=~/dotfiles_old                   # old dotfiles backup directory
-files="bashrc vimrc vim pythonrc.py gtkrc-2.0"    # list of files/folders to symlink in homedir
+# dotfiles directory
+dir=~/dotfiles
+# old dotfiles backup directory
+olddir=~/dotfiles_old
+# list of files/folders to symlink in homedir
+files="bashrc vimrc vim pythonrc.py gtkrc-2.0 gitconfig"
 
 ##########
 
